c9001d5260325d068f3fef0e0cceb47c5ae54d75
schrodlm <msamot@seznam.cz>
2023-12-13 17:10:07 +0100

    docs/source_code_doc

--- src/main/scala/Config/ImageOutput.scala
+++ src/main/scala/Config/ImageOutput.scala
@@ -1,5 +1,24 @@
 package Config
 
+/**
+ * Represents the output destination for generated ASCII art.
+ *
+ * This sealed trait is used as a base to define different types of output destinations
+ * for ASCII art, such as to a file or to the console.
+ */
 sealed trait ImageOutput
+
+/**
+ * Represents an ASCII art output to a specified file path.
+ *
+ * @param path the file path where the ASCII art will be saved.
+ */
 case class PathImageOutput(path: String) extends ImageOutput
+
+/**
+ * Represents an ASCII art output to the console.
+ *
+ * This case class is used when the ASCII art is intended to be printed directly
+ * to the standard console output.
+ */
 case class ConsoleImageOutput() extends ImageOutput
