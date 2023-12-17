3317541cf2ee3a140c8f8ac42c4994c0604b1e1d
Matěj Schrödl <msamot@seznam.cz>
2023-12-17 14:00:45 +0100

    fix/refactor_tables

--- src/main/scala/Conversion/GrayscaleToAsciiConverter.scala
+++ src/main/scala/Conversion/GrayscaleToAsciiConverter.scala
@@ -1,44 +1,16 @@
 package Conversion
-import Config.{LinearTable, NonLinearTable, Table}
+import Config.Table
 import Image.{AsciiArt, GrayscaleImage, Image}
 
 class GrayscaleToAsciiConverter(){
 
-  def convert(image : GrayscaleImage, table: LinearTable) : AsciiArt = {
-
-    val width = image.getWidth()
-    val height = image.getHeight()
-
-    val bars = table.chars.length
+  def convert(image : GrayscaleImage, table: Table) : AsciiArt = {
 
     val ascii_data =  image.getData().map { value =>
         val grayscaleValue = value & 0xFF
         table.getChar(grayscaleValue).toInt
-
     }
 
-    new AsciiArt(width, height, ascii_data)
-  }
-
-  def convert(image: GrayscaleImage, table: NonLinearTable): AsciiArt = {
-
-    val width = image.getWidth()
-    val height = image.getHeight()
-
-    val bars = table.chars.length
-
-    val ascii_data = image.getData().map { value =>
-      val grayscaleValue = value & 0xFF
-      table.getChar(grayscaleValue).toInt
-
-
-    }
-
-    new AsciiArt(width, height, ascii_data)
-  }
-
-  protected def convertWithLinearTable(): Unit =
-  {
-
+    new AsciiArt(image.getWidth(), image.getHeight(), ascii_data)
   }
 }
