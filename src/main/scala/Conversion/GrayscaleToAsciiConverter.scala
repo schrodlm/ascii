97b7af85652259a797813e3244a504d82a2c90ec
Matěj Schrödl <msamot@seznam.cz>
2023-12-17 13:06:49 +0100

    feat/ascii_conversion

--- src/main/scala/Conversion/GrayscaleToAsciiConverter.scala
+++ src/main/scala/Conversion/GrayscaleToAsciiConverter.scala
@@ -1,7 +1,22 @@
 package Conversion
+import Config.Table
 import Image.{AsciiArt, GrayscaleImage, Image}
 
 class GrayscaleToAsciiConverter(){
 
-  def convert(image : GrayscaleImage) : AsciiArt = ???
+  def convert(image : GrayscaleImage, table: Table) : AsciiArt = {
+
+    val width = image.getWidth()
+    val height = image.getHeight()
+
+    val bars = table.chars.length
+
+    val ascii_data =  image.getData().map { value =>
+        val grayscaleValue = (value & 0xFF)
+        val index = (bars-1) * ( grayscaleValue / 255.0)
+        table.chars(index.toInt).toInt
+    }
+
+    new AsciiArt(width, height, ascii_data)
+  }
 }
