10cb7da20c64c04c5bb8e8ab9d8b00e374682b25
Matěj Schrödl <msamot@seznam.cz>
2023-12-21 19:26:23 +0100

    fix/refactor_scale

--- src/main/scala/Filter/ScaleFilter.scala
+++ src/main/scala/Filter/ScaleFilter.scala
@@ -1,21 +1,48 @@
 package Filter
+
 import Image.AsciiArt
 
-class ScaleFilter(val scale : Int) extends AsciiArtFilter {
+class ScaleFilter(val scale: Double) extends AsciiArtFilter {
   override def apply(image: AsciiArt): AsciiArt = {
     if (scale <= 0) throw new IllegalArgumentException("Scale must be positive")
 
-    val newWidth = image.getWidth() / scale
-    val newHeight = image.getHeight() / scale
+    if (scale >= 1) {
+      scaleDown(image)
+    }
+
+    else {
+      scaleUp(image)
+    }
+  }
+
+  private def scaleDown(image: AsciiArt): AsciiArt = {
+
+    val newWidth = (image.getWidth() / scale).toInt
+    val newHeight = (image.getHeight() / scale).toInt
+
+    val newData = (0 until newHeight).flatMap { y =>
+      (0 until newWidth).map { x =>
+        val originalX = x * scale.toInt
+        val originalY = y * scale.toInt
+        image.getData()(originalY * image.getWidth() + originalX)
+      }
+    }.toArray
+
+    new AsciiArt(newWidth, newHeight, newData)
+  }
+
+  private def scaleUp(image: AsciiArt): AsciiArt = {
+    val pixelRatio = (1 / scale).toInt
+    val newWidth = (image.getWidth() / scale).toInt
+    val newHeight = (image.getHeight() / scale).toInt
     val newData = Array.ofDim[Int](newWidth * newHeight)
 
-    for {
-      y <- 0 until newHeight
-      x <- 0 until newWidth
-    } {
-      val originalX = x * scale
-      val originalY = y * scale
-      newData(y * newWidth + x) = image.getData()(originalY * image.getWidth() + originalX)
+    (0 until image.getHeight()).foreach { y =>
+      (0 until image.getWidth()).foreach { x =>
+        (0 until pixelRatio).foreach { pixelCount =>
+          newData((y * image.getWidth() + x) * pixelRatio + pixelCount) = image.getData()(y * image.getWidth() + x)
+        }
+      }
     }
 
     new AsciiArt(newWidth, newHeight, newData)
